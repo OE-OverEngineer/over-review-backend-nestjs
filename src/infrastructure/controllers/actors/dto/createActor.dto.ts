c13cfa16dcce57f4bd44553a817730d78fcc178c
thekogo <thekogo@gmail.com>
2021-11-20 07:11:31 +0700

    refactor: delete unused dto in controller

--- src/infrastructure/controllers/actors/dto/createActor.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CreateActorDto {
-  @ApiProperty()
-  firstName: string;
-  @ApiProperty()
-  lastName: string;
-}
--- src/infrastructure/controllers/category/dto/createCategory.dto.ts
+++
@@ -1,6 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CreateCategoryDto {
-  @ApiProperty()
-  title: string;
-}
