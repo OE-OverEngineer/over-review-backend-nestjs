9842f21e9c3c8b5683e1a289fbdf8eefd708d8c8
thekogo <thekogo@gmail.com>
2021-11-20 07:09:21 +0700

    test: add role test case

--- src/infrastructure/dto/roles/createRole.dto.spec.ts
+++ src/infrastructure/dto/roles/createRole.dto.spec.ts
@@ -1,17 +1,67 @@
 import { ArgumentMetadata, ValidationPipe } from '@nestjs/common';
+import { json } from 'stream/consumers';
 import { CreateRoleDto } from './createRole.dto';
 
-it('validate DTO', async () => {
-  const target: ValidationPipe = new ValidationPipe({
-    transform: true,
-    whitelist: true,
+describe('Validate createRole dto', () => {
+  it('validate title is correct', async () => {
+    const target: ValidationPipe = new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    });
+    const metadata: ArgumentMetadata = {
+      type: 'body',
+      metatype: CreateRoleDto,
+    };
+    await target
+      .transform(
+        <CreateRoleDto>{
+          title: 'superadmin',
+        },
+        metadata,
+      )
+      .catch((err) => {
+        expect(err.getResponse().message).toEqual([]);
+      });
   });
-  const metadata: ArgumentMetadata = {
-    type: 'body',
-    metatype: CreateRoleDto,
-    data: '',
-  };
-  await target.transform(<CreateRoleDto>{}, metadata).catch((err) => {
-    expect(err.getResponse().message).toEqual(['title must be a string']);
+
+  it('validate title is not empty', async () => {
+    const target: ValidationPipe = new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    });
+    const metadata: ArgumentMetadata = {
+      type: 'body',
+      metatype: CreateRoleDto,
+    };
+    await target.transform(<CreateRoleDto>{}, metadata).catch((err) => {
+      expect(err.getResponse().message).toEqual([
+        'title must be a string',
+        'title should not be empty',
+      ]);
+    });
+  });
+
+  it('validate title is not undefined', async () => {
+    const target: ValidationPipe = new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    });
+    const metadata: ArgumentMetadata = {
+      type: 'body',
+      metatype: CreateRoleDto,
+    };
+    await target
+      .transform(
+        <CreateRoleDto>{
+          title: undefined,
+        },
+        metadata,
+      )
+      .catch((err) => {
+        expect(err.getResponse().message).toEqual([
+          'title must be a string',
+          'title should not be empty',
+        ]);
+      });
   });
 });
