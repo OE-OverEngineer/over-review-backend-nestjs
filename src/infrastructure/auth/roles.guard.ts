ee5e3484d8630818c968cd7c3988c70c804869be
721945 <this721945@gmail.com>
2021-12-07 02:43:19 +0700

    refactor: remove console log from role guard

--- src/infrastructure/auth/roles.guard.ts
+++ src/infrastructure/auth/roles.guard.ts
@@ -17,7 +17,6 @@ export class RolesGuard implements CanActivate {
     }
     const request = context.switchToHttp().getRequest();
     const user = request.user;
-    console.log(user);
     const hasRole = (): boolean =>
       requiredRoles.some((role) => role === user.role);
     return user && user.role && hasRole();
