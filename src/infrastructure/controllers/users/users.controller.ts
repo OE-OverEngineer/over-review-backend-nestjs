3fa0904b89412f9475d82377d4f06c014be6425d
thekogo <thekogo@gmail.com>
2021-12-07 03:50:30 +0700

    docs: add user swagger doc

--- src/infrastructure/controllers/users/users.controller.ts
+++ src/infrastructure/controllers/users/users.controller.ts
@@ -16,7 +16,13 @@ import { JwtAuthGuard } from 'src/infrastructure/auth/jwt-auth.guard';
 
 import { ReviewsUseCases } from 'src/usecases/reviews/reviews.usecase';
 import { Pagination } from 'src/infrastructure/dto/pagination/pagination.dto';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { Role } from 'src/infrastructure/auth/role.enum';
 import { Roles } from 'src/infrastructure/auth/roles.decorator';
 import { RolesGuard } from 'src/infrastructure/auth/roles.guard';
@@ -35,29 +41,37 @@ export class UsersController {
   ) {}
 
   @ApiBearerAuth('access-token')
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles(Role.Admin)
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userUsecases.create(createUserDto);
-  }
-
-  @ApiBearerAuth('access-token')
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles(Role.Admin)
-  @Get()
-  findAll() {
-    return this.userUsecases.findAll();
-  }
-
-  @ApiBearerAuth('access-token')
   @UseGuards(JwtAuthGuard)
-  // @Roles(Role.Member)
+  @Roles(Role.Member)
+  @ApiUnauthorizedResponse({
+    description: 'Member missing or wrong jwt',
+  })
+  @ApiOkResponse({
+    description: 'Get current member profile',
+  })
   @Get('profile')
   async me(@Request() req) {
     console.log(req);
     const user = await this.userUsecases.findOne(req.user.id);
     return user;
+  }
+
+  @ApiBearerAuth('access-token')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Member)
+  @ApiUnauthorizedResponse({
+    description: 'Member missing or wrong jwt',
+  })
+  @ApiOkResponse({
+    description: 'Update profile success',
+  })
+  @ApiBadRequestResponse({
+    description: 'Data is incorrect format',
+  })
+  @Patch('/profile')
+  async updateByIdToken(@Request() req: any, @Body() dto: UpdateProfileDto) {
+    const userId = Number(req.user.id);
+    return this.userUsecases.update(userId, dto);
   }
 
   @Get('/:id/reviews')
@@ -66,24 +80,6 @@ export class UsersController {
     @Param('id') id: number,
   ) {
     return this.reviewsUsecases.findAllByUserID(id, pagintaion);
-  }
-
-  @ApiBearerAuth('access-token')
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles(Role.Member)
-  @Patch('/profile')
-  async updateByIdToken(@Request() req: any, @Body() dto: UpdateProfileDto) {
-    const userId = Number(req.user.id);
-    return this.userUsecases.update(userId, dto);
-  }
-
-  @ApiBearerAuth('access-token')
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles(Role.Member)
-  @Patch('/:id')
-  async updateUser(@Request() req: any, @Body() dto: UpdateUserDto) {
-    const userId = Number(req.user.id);
-    return this.userUsecases.update(userId, dto);
   }
 
   @Get('/top-review/')
@@ -108,13 +104,7 @@ export class UsersController {
     const userId = Number(id);
     return this.userUsecases.banUser(userId, false);
   }
-  // @UseGuards(JwtAuthGuard)
-  // @ApiBearerAuth('access-token')
-  // @Get('profile')
-  // @Get('/liked-review/')
-  // async findLikedReview(@Request() req: any) {
-  //   return this.userUsecases.findTopReviewers(amount);
-  // }
+
   @UseGuards(JwtAuthGuard, RolesGuard)
   @ApiBearerAuth('access-token')
   @Roles(Role.Member)
@@ -122,30 +112,6 @@ export class UsersController {
   async findLikedReview(@Request() req: any) {
     const userID = Number(req.user.id);
     return this.likeUsecases.findLikeByUserID(userID);
-  }
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.usersService.findById(Number.parseInt(id));
-  // }
-
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-  //   return this.usersService.update(+id, updateUserDto);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.usersService.deleteById(+id);
-  // }
-
-  // async reportUser() {
-
-  // }
-
-  @Get('/:id')
-  viewProfile(@Param('id') id: number) {
-    const userId = Number(id);
-    return this.userUsecases.findOne(userId);
   }
 
   @UseGuards(JwtAuthGuard, RolesGuard)
@@ -155,5 +121,36 @@ export class UsersController {
   deleteUser(@Param('id') id: number) {
     const userId = Number(id);
     return this.userUsecases.delete(userId);
+  }
+
+  @ApiBearerAuth('access-token')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
+  @Get()
+  findAll() {
+    return this.userUsecases.findAll();
+  }
+
+  @ApiBearerAuth('access-token')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userUsecases.create(createUserDto);
+  }
+
+  @Get('/:id')
+  viewProfile(@Param('id') id: number) {
+    const userId = Number(id);
+    return this.userUsecases.findOne(userId);
+  }
+
+  @ApiBearerAuth('access-token')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Member)
+  @Patch('/:id')
+  async updateUser(@Request() req: any, @Body() dto: UpdateUserDto) {
+    const userId = Number(req.user.id);
+    return this.userUsecases.update(userId, dto);
   }
 }
